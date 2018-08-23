3885312d1093224f9a50bcaca9892914c870b34e
Saracen <SaracenOne@gmail.com>
2018-08-23 08:30:20 +0100

    Fix BlendShapeData resource.

--- blend_shape_data.gd
+++ blend_shape_data.gd
@@ -4,9 +4,9 @@ tool
 # Todo: cannot serialize this as a subresource from another resource. Bug?
 class BlendShapeSurface extends Resource:
 	export(String) var name = ""
-	export(PoolIntArray) var index_array = IntArray()
-	export(PoolVector3Array) var position_array = Vector3Array()
-	export(PoolVector3Array) var normal_array = Vector3Array()
+	export(PoolIntArray) var index_array = PoolIntArray()
+	export(PoolVector3Array) var position_array = PoolVector3Array()
+	export(PoolVector3Array) var normal_array = PoolVector3Array()
 
 export(String) var name = ""
 export(Array) var surfaces = []
