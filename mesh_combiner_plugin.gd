123ba78811448badd7af3f2e5fc83ce6eaf28444
Saracen <SaracenOne@gmail.com>
2020-08-16 22:14:47 +0100

    Add printed messages for initialisation and destruction.

--- mesh_combiner_plugin.gd
+++ mesh_combiner_plugin.gd
@@ -2,5 +2,15 @@ tool
 extends EditorPlugin
 
 
+func _init() -> void:
+	print("Initialising MeshCombiner plugin")
+
+
+func _notification(p_notification: int):
+	match p_notification:
+		NOTIFICATION_PREDELETE:
+			print("Destroying MeshCombiner plugin")
+
+
 func get_name() -> String:
 	return "MeshCombiner"
